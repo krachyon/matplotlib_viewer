b981c5b6c44bc9c9283b294fdfbecfdd6ae2fd27
Sebastian <sebastian.messlinger@posteo.de>
2021-01-28 13:31:45 +0100

    implemented arrow navigation

--- README.md
+++ README.md
@@ -12,3 +12,6 @@ with open(filename_base+'.mplf', 'wb') as outfile:
 Put matplotlib_viewer.py somewhere in your path and configure your system to open `.mplf`-files with it.
 
 Clicking on a mplf file should load and display it
+
+You can use the left and right arrow keys to navigate between figures in the directory the original figure was in.
+This probably only works with QT backend...
--- matplotlib_viewer.py
+++ matplotlib_viewer.py
@@ -3,9 +3,43 @@
 import matplotlib.pyplot as plt
 import pickle
 import sys
+import glob
+import os
 
-with open(sys.argv[1], 'rb') as f:
-    fig = pickle.load(f)
 
-fig.show()
+def press(event):
+    global current_index
+    print('press')
+
+    if event.key == 'left':
+        current_index -= 1
+    elif event.key == 'right':
+        current_index += 1
+    else:
+        return
+
+    display(figure_files[current_index % len(figure_files)])
+
+
+def display(figure_file):
+    old_fig = plt.gcf()
+    geometry = plt.get_current_fig_manager().window.geometry()
+
+    with open(figure_file, 'rb') as f:
+        fig = pickle.load(f)
+
+    fig.show()
+    plt.get_current_fig_manager().window.setGeometry(geometry)
+    plt.close(old_fig)
+    plt.gcf().canvas.mpl_connect('key_press_event', press)
+
+
+file_to_open = sys.argv[1]
+
+working_dir = os.path.dirname(file_to_open)
+figure_files = glob.glob(os.path.join(os.path.abspath(working_dir), '*.mplf'))
+
+current_index = figure_files.index(file_to_open)
+
+display(file_to_open)
 plt.show()
